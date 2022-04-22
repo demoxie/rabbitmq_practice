cb25763c76a8ea01f63c6787995653e292416427
Shadrach Adamu <shadrachadamuul@gmail.com>
2022-04-22 19:26:34 +0100

    worked

---
+++ producer/src/main/java/rabbitmq/producer/entity/Picture.java
@@ -0,0 +1,15 @@
+package rabbitmq.producer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Picture {
+    private String name;
+    private String type;
+    private String source;
+    private Long size;
+}
